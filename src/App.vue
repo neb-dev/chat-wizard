686f48395f438ba3a24cf184d9aa69c7e7bd4377
neb <10528790@my.uvu.edu>
2021-03-05 22:03:35 -0700

    added dracula colors to custom theme, layout adjustments and styling updates

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app class="accent">
       <notepad />
       <notepad />
       <notepad />
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app flat class="accent">
       <!-- -->
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
-    <v-main>
+    <v-main class="primary">
       <!-- Provides the application the proper gutter -->
       <v-container fluid> </v-container>
     </v-main>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
@@ -35,3 +31,9 @@ export default {
   })
 };
 </script>
+
+<style scoped>
+/* #app {
+  background-color: var(--v-background-base);
+} */
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,25 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  theme: { dark: true }
+  theme: {
+    options: {
+      customProperties: true
+    },
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#282A36",
+        accent: "#44475a",
+        secondary: "#FF79C6",
+        success: "#50FA7B",
+        info: "#8BE9FD",
+        warning: "#BD93F9",
+        error: "#FF5555"
+        // background: "#282A36"
+      }
+    }
+  }
 });
