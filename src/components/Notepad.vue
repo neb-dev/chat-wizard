1aad146ba253fb5d801652f595afbb14770dfad6
neb <10528790@my.uvu.edu>
2021-03-06 20:03:58 -0700

    fixed margin and padding, updated input interaction, replaced with theme colors

--- src/components/Notepad.vue
+++ src/components/Notepad.vue
@@ -1,78 +1,89 @@
 <template>
   <div class="notepad">
-    <v-card class="my-2 py-3 primary">
-      <v-form class="my-0 py-0">
-        <v-container class="my-0 py-0">
-          <v-row class="my-0 py-0">
+    <v-card tile class="background">
+      <v-form>
+        <v-container>
+          <v-row class="mt-0 mb-1 py-0">
             <v-col class="my-0 py-0">
               <v-text-field
                 v-model="name"
                 label="name"
                 clearable
-                class="my-1 py-1"
+                class="my-0 py-0"
                 dense
-                height="20px"
+                single-line
               ></v-text-field>
             </v-col>
           </v-row>
-          <v-row class="my-0 py-0">
+          <v-row class="my-1 py-0">
             <v-col class="my-0 py-0">
               <v-switch
                 v-model="verified"
                 inset
                 :label="`verified: ${verified.toString()}`"
-                color="green accent-3"
-                class="mt-0 mb-1 pa-0"
+                color="success"
+                class="my-0 py-0"
                 dense
-                height="10px"
+                height="5px"
               ></v-switch>
             </v-col>
           </v-row>
-          <v-row class="my-0 py-0">
+          <v-row class="my-1 py-0">
             <v-col class="my-0 py-0">
               <v-textarea
                 v-model="issue"
                 label="issue"
                 rows="1"
                 class="my-0 py-0"
+                auto-grow
+                clearable
+                dense
+                single-line
               ></v-textarea>
             </v-col>
           </v-row>
-          <v-row class="my-0 py-0">
+          <v-row class="my-1 py-0">
             <v-col class="my-0 py-0">
               <v-textarea
                 v-model="ts"
                 label="troubleshooting"
                 rows="1"
                 class="my-0 py-0"
+                auto-grow
+                clearable
+                dense
+                single-line
               ></v-textarea>
             </v-col>
           </v-row>
-          <v-row class="my-0 py-0">
+          <v-row class="my-1 py-0">
             <v-col class="my-0 py-0">
-              <v-text-field
+              <v-textarea
                 v-model="resolution"
                 label="resolution"
-                clearable
+                rows="1"
                 class="my-0 py-0"
-                height="20px"
-              ></v-text-field>
+                auto-grow
+                clearable
+                dense
+                single-line
+              ></v-textarea>
             </v-col>
           </v-row>
-          <v-row class="my-0 py-0">
+          <v-row class="my-1 py-0">
             <v-col class="my-0 py-0">
               <v-switch
                 v-model="referral"
                 inset
                 :label="`refferal: ${referral.toString()}`"
-                color="green accent-3"
-                class="mt-0 mb-1 pa-0"
+                color="success"
+                class="my-0 py-0"
                 dense
-                height="10px"
+                height="5px"
               ></v-switch>
             </v-col>
           </v-row>
-          <v-row class="text-center my-0 py-0">
+          <v-row class="text-center mt-1 mb-0 py-0">
             <v-col class="my-0 py-0">
               <v-btn small color="success" class="my-0 py-0" height="25px">
                 <v-icon dense>mdi-clipboard-text-outline</v-icon>
