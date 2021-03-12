6d4c1f4bdd858c7e1fddd1c010bc87b09f40b327
neb <jneb.dev@outlook.com>
2021-03-11 19:20:21 -0700

    added scriptgrid component

---
+++ src/components/script/ScriptGrid.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-button>Script 1</v-button>
+                <v-button>Script 2/v-button>
+                <v-button>Script 3</v-button>
+                <v-button>Script 4</v-button>
+                <v-button>Script 5</v-button>
+                <v-button>Script 6</v-button>
+                <v-button>Script 7</v-button>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
